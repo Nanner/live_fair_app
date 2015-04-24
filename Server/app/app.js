f539528195a555845ea2ad1473314ba321992da5
PMRocha <ihastealth@gmail.com>
2015-04-24 11:42:52 +0100

    initial database defenition (lacks table relations)

--- Server/app/app.js
+++ Server/app/app.js
@@ -1,12 +1,114 @@
-'use strict';
+var Hapi = require('hapi');
+var Good = require('good');
+var Sequelize = require('sequelize');
 
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+var sequelize = new Sequelize('PostgreSQL 9.4', 'admin', 'coisas', {
+  host: 'localhost',
+  dialect: 'postgres',
+
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  },
+
+  // SQLite only
+  storage: 'path/to/database.sqlite'
+});
+
+var server = new Hapi.Server();
+server.connection({ port: 3000 });
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply('Hello, world!');
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/{name}',
+    handler: function (request, reply) {
+        reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+    }
+});
+
+server.register({
+    register: Good,
+    options: {
+        reporters: [{
+            reporter: require('good-console'),
+            events: {
+                response: '*',
+                log: '*'
+            }
+        }]
+    }
+}, function (err) {
+    if (err) {
+        throw err; // something bad happened loading the plugin
+    }
+
+    server.start(function () {
+        server.log('info', 'Server running at: ' + server.info.uri);
+    });
+});
+
+var User = sequelize.define('user', {
+  username: {
+    type: Sequelize.STRING,
+  },
+  password: {
+    type: Sequelize.STRING
+  },
+  type: {
+    type: Sequelize.STRING
+  },
+  contact: {
+    type: Sequelize.STRING
+  },
+  address: {
+    type: Sequelize.TEXT
+  },
+  description: {
+    type: Sequelize.TEXT
+  },
+  blocked: {
+    type: Sequelize.BOOLEAN
+  }
+}, {
+  freezeTableName: true // Model tableName will be the same as the model name
+});
+
+var Organizer = sequelize.define('organizer', {
+}, {
+  freezeTableName: true // Model tableName will be the same as the model name
+});
+
+var Company = sequelize.define('company', {
+  username: {
+    type: Sequelize.STRING,
+  },
+  password: {
+    type: Sequelize.STRING
+  },
+  type: {
+    type: Sequelize.STRING
+  },
+  contact: {
+    type: Sequelize.STRING
+  },
+  address: {
+    type: Sequelize.TEXT
+  },
+  description: {
+    type: Sequelize.TEXT
+  },
+  blocked: {
+    type: Sequelize.BOOLEAN
+  }
+}, {
+  freezeTableName: true // Model tableName will be the same as the model name
+});
