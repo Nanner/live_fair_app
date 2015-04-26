8110b6c01cb7576886ecc5b12918e0bf39ce1524
PMRocha <ei11078@fe.up.pt>
2015-04-26 16:33:33 +0100

    most relations/ routes are done
    need to find way to make custom relationship tables

--- Server/app/app.js
+++ Server/app/app.js
@@ -11,9 +11,6 @@ var sequelize = new Sequelize('PostgreSQL 9.4', 'admin', 'coisas', {
     min: 0,
     idle: 10000
   },
-
-  // SQLite only
-  storage: 'path/to/database.sqlite'
 });
 
 var server = new Hapi.Server();
@@ -21,15 +18,24 @@ server.connection({ port: 3000 });
 
 server.route({
     method: 'GET',
-    path: '/',
+    path: '/lifefairs',
     handler: function (request, reply) {
-        reply('Hello, world!');
+      reply(LiveFair.findAll({order: 'liveFairID DESC'}).then(function(liveFairs)
+      {
+        JSON.stringify(liveFairs);
+      }));
     }
 });
 
 server.route({
     method: 'GET',
-    path: '/{name}',
+    path: '/lifefairs/{id}',
+    handler: function (request, reply) {
+      reply(LiveFair.find({request.params.id}).then(function(liveFair)
+      {
+        JSON.stringify(liveFair);
+      }));
+    }
     handler: function (request, reply) {
         reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
     }
@@ -57,8 +63,12 @@ server.register({
 });
 
 var User = sequelize.define('user', {
+  userID:{
+    type: Sequelize.UUID,
+    primaryKey: true
+  },
   username: {
-    type: Sequelize.STRING,
+    type: Sequelize.STRING, unique: true
   },
   password: {
     type: Sequelize.STRING
@@ -67,10 +77,10 @@ var User = sequelize.define('user', {
     type: Sequelize.STRING
   },
   contact: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING, unique: true
   },
   address: {
-    type: Sequelize.TEXT
+    type: Sequelize.TEXT, unique: true
   },
   description: {
     type: Sequelize.TEXT
@@ -83,19 +93,24 @@ var User = sequelize.define('user', {
 });
 
 var Organizer = sequelize.define('organizer', {
+  organizerName: {
+  type: Sequelize.STRING, unique: true
+}
 }, {
   freezeTableName: true // Model tableName will be the same as the model name
 });
 
+Organizer.belongsTo(User, {foreignKey: 'organizerID'});
+
 var Company = sequelize.define('company', {
   companyName: {
-    type: Sequelize.STRING,
+    type: Sequelize.STRING, unique: true
   },
   logoImage: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING, unique: true
   },
   website: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING, unique: true
   },
   visitorCounter: {
     type: Sequelize.INTEGER
@@ -104,16 +119,25 @@ var Company = sequelize.define('company', {
   freezeTableName: true // Model tableName will be the same as the model name
 });
 
+Company.belongsTo(User, {foreignKey: 'companyID'});
+
 var Visitor = sequelize.define('visitor', {
   Photo: {
-    type: Sequelize.STRING,
+    type: Sequelize.STRING, unique: true
+  }
 }, {
   freezeTableName: true // Model tableName will be the same as the model name
 });
 
+Visitor.belongsTo(User, {foreignKey: 'visitorID'});
+
 var LiveFair = sequelize.define('liveFair', {
+  liveFairID:{
+    type: Sequelize.UUID,
+    primaryKey: true
+  },
   name: {
-    type: Sequelize.STRING,
+    type: Sequelize.STRING, unique: true
   },
   description: {
     type: Sequelize.STRING
@@ -131,9 +155,13 @@ var LiveFair = sequelize.define('liveFair', {
   freezeTableName: true // Model tableName will be the same as the model name
 });
 
+Visitor.belongsToMany(LiveFair, {through: "visitor_liveFair"});
+LiveFair.belongsToMany(Visitor, {through: "visitor_liveFair"});
+Organizer.hasMany(LiveFair);
+
 var LiveFairEvents = sequelize.define('liveFairEvents', {
   eventLocation: {
-    type: Sequelize.STRING,
+    type: Sequelize.STRING
   },
   time: {
     type: Sequelize.DATE
@@ -147,3 +175,52 @@ var LiveFairEvents = sequelize.define('liveFairEvents', {
 }, {
   freezeTableName: true // Model tableName will be the same as the model name
 });
+
+LiveFair.hasMany(LiveFairEvents, {foreignKey: 'liveFairEventsID'});
+
+var Interest = sequelize.define('interest', {
+  interestID:{
+    type: Sequelize.UUID,
+    primaryKey: true
+  },
+  interest:{
+    type: Sequelize.STRING
+  }
+}, {
+  freezeTableName: true // Model tableName will be the same as the model name
+});
+
+var CompanyEvents = sequelize.define('companyEvents', {
+  companyEventsID:{
+    type: Sequelize.UUID,
+    primaryKey: true
+  },
+  location:{
+    type: Sequelize.TEXT
+  },
+  time:{
+    type: Sequelize.DATE
+  },
+  speakers:{
+    type: Sequelize.TEXT
+  }
+}, {
+  freezeTableName: true // Model tableName will be the same as the model name
+});
+
+//Ternary relation definition Company-CompanyEvents-LiveFair
+Company.belongsToMany(LiveFair, {through: "company_companyEvents_liveFair"});
+CompanyEvents.belongsToMany(LiveFair, {through: "company_companyEvents_liveFair"});
+CompanyEvents.belongsToMany(Visitor, {through: "company_companyEvents_liveFair"});
+
+//Ternary relation definition Company-Interest-LiveFair
+Company.belongsToMany(LiveFair, {through: "company_interest_liveFair"});
+Interest.belongsToMany(LiveFair, {through: "company_interest_liveFair"});
+Interest.belongsToMany(Company, {through: "company_interest_liveFair"});
+
+//Ternary relation definition Visitor-Interest-LiveFair
+Visitor.belongsToMany(LiveFair, {through: "visitor_interest_liveFair"});
+Interest.belongsToMany(Visitor, {through: "visitor_interest_liveFair"});
+Interest.belongsToMany(LiveFair, {through: "visitor_interest_liveFair"});
+
+sequelize.sync({force: true});
