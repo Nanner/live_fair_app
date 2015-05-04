2f0420dcb525706397e1bae245cb321d80b7d6a6
PMRocha <ei11078@fe.up.pt>
2015-05-04 16:48:03 +0100

    changed routes to various files

--- Server/app/app.js
+++ Server/app/app.js
@@ -16,42 +16,7 @@ var sequelize = new Sequelize('liveFairAppDB', 'postgres', 'root', {
 var server = new Hapi.Server();
 server.connection({ port: 3000 });
 
-server.route({
-    method: 'GET',
-    path: '/lifefairs',
-    handler: function (request, reply) {
-      reply(LiveFair.findAll({order:'liveFairID DESC'}).then(function(liveFairs)
-      {
-        return JSON.stringify(liveFairs);
-      }));
-    }
-});
-
-server.route({
-    method: 'GET',
-    path: '/lifefairs/{id}',
-    handler: function (request, reply) {
-        var liveFairId=request.params.id;
-        reply(LiveFair.find(liveFairId).then(function(liveFair)
-      {
-        return JSON.stringify(liveFair);
-      }));
-    }
-});
-
-server.route({
-    method: 'GET',
-    path: '/lifefairs/{id}/schedule',
-    handler: function (request, reply) {
-      var liveFairId=request.params.id;
-      reply(LiveFairEvents.find({where:
-                                      { liveFairEventsID: liveFairId
-                                          }}).then(function(liveFairEvents)
-                                        {
-        return JSON.stringify(liveFairEvents);
-      }));
-    }
-});
+require('./routes/liveFair')(server);
 
 server.register({
     register: Good,
---
+++ Server/app/routes/liveFair.js
@@ -0,0 +1,38 @@
+module.exports = function(server){
+server.route({
+    method: 'GET',
+    path: '/lifefairs',
+    handler: function (request, reply) {
+      reply(LiveFair.findAll({order:'liveFairID DESC'}).then(function(liveFairs)
+      {
+        return JSON.stringify(liveFairs);
+      }));
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/lifefairs/{id}',
+    handler: function (request, reply) {
+        var liveFairId=request.params.id;
+        reply(LiveFair.find(liveFairId).then(function(liveFair)
+      {
+        return JSON.stringify(liveFair);
+      }));
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/lifefairs/{id}/schedule',
+    handler: function (request, reply) {
+      var liveFairId=request.params.id;
+      reply(LiveFairEvents.find({where:
+                                      { liveFairEventsID: liveFairId
+                                          }}).then(function(liveFairEvents)
+                                        {
+        return JSON.stringify(liveFairEvents);
+      }));
+    }
+});
+};
