339440e23d48fd452dc5430f5ccbb18046d74b8b
PMRocha <ei11078@fe.up.pt>
2015-05-13 17:21:36 +0100

    [server] login and register done, linkedin(WIP)
    need to try and change route files

--- Server/app/routes/user.js
+++ Server/app/routes/user.js
@@ -1,65 +1,39 @@
 var Promise = require("bluebird");
 var crypto = require("crypto");
+var uuid = require('node-uuid');
 
 var User = require('../models').User;
 
 module.exports = function(server){
-    server.route({
-        method: 'POST',
-        path: '/login',
-        config:{
-            handler: function (request, reply) {
-                if (request.auth.isAuthenticated) {
-                    return reply.redirect('/');
-                }
-
-                var message = '';
-                var account = null;
-
-                if (request.method === 'post') {
-                    if (!request.payload.username ||
-                        !request.payload.password) {
-                        message = 'Missing username or password';
-                    }
-                    else {
-                        User.find({where:
-                        {
-                            email: request.payload.username
-                        }}).then(function(account) {
-                            if(!account) {
-                                message = 'Invalid username or password';
-                            }
-                            else {
-                                var hash = crypto.createHash('sha512');
-                                hash.update(account.password);
-                                if (hash.digest() !== request.payload.password) {
-                                    message = 'Invalid username or password';
-                                }
-                                else {
-                                    request.auth.session.set(account);
-                                }
-                            }
-                        });
-                    }
-                }
-
-                return JSON.stringify("temporary");
-
-            },
-            plugins: {
-                'hapi-auth-cookie': {
-                    redirectTo: false
-                }
-            }
-        }
-    });
 
     server.route({
         method: 'GET',
         path: '/logout',
         handler: function (request, reply) {
             request.auth.session.clear();
-            return reply.redirect('/');
+            return JSON.stringify('Logout bem sucedido');
         }
+    });
+    
+    server.route({
+       method: 'POST',
+       path: '/register',
+       handler: function(request,reply){
+           if(!request.payload.email || !request.payload.password || !request.payload.type){
+               reply(JSON.stringify('Missing critical fields'));
+           }
+           else{
+               var passHash=crypto.createHash('sha512');
+               passHash.update(request.payload.password);
+               User.create({
+                   'userID':uuid.v4(),
+                    'email':request.payload.email,
+                    'password':passHash.digest(),
+                    'type':request.payload.type
+                    } 
+               );
+              reply(JSON.stringify('Registo Bem Sucedido'));
+           }
+       }     
     });
 };
