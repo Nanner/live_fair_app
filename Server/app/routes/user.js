e06c74170242a6152778ad32150c5e3896cec2b0
Diogo Santos <diogo.rgsantos@gmail.com>
2015-05-15 22:49:29 +0100

    [Server] Fixed the register route. It wasn't considering that promises are asynchronous and it would fail if the create company action happened before the create user one. Also implemented a transaction to make sure the create user action rolls back if the create company one fails.

--- Server/app/routes/user.js
+++ Server/app/routes/user.js
@@ -2,11 +2,14 @@ var Promise = require("bluebird");
 var crypto = require("crypto");
 var uuid = require('node-uuid');
 
+var sequelize = require('../models').sequelize;
+
 var User = require('../models').User;
 
 var Company = require('../models').Company;
 var Visitor = require('../models').Visitor;
 var Organizer = require('../models').Organizer;
+var Boom = require('boom');
 
 module.exports = function(server){
 
@@ -18,59 +21,66 @@ module.exports = function(server){
             return JSON.stringify('Logout bem sucedido');
         }
     });
-    
+
     server.route({
-       method: 'POST',
-       path: '/register',
-       handler: function(request,reply){
-           if(!request.payload.email || !request.payload.password || !request.payload.type){
-               reply(JSON.stringify('Missing critical fields'));
-           }
-           else{
-               var passHash=crypto.createHash('sha512');
-               var ID=uuid.v4();
-               passHash.update(request.payload.password);
-               User.create({
-                    'userID':ID,
-                    'email':request.payload.email,
-                    'password':passHash.digest(),
-                    'type':request.payload.type
-                    } 
-               );
-              
-              switch (request.payload.type){
-                  case 'company':
-                        var obj = {
-                            'companyID':ID,
-                            'visitorCounter': 0
-                        };
-                        
-                        if(request.payload.address)
-                            obj['address']=request.payload.address;
-                            
-                        if(request.payload.companyName)
-                            obj['companyName']=request.payload.companyName;
-                            
-                        if(request.payload.website)
-                            obj['website']=request.payload.website;
-                            
-                        Company.create(obj);
-                        reply(JSON.stringify('Registo Bem Sucedido'));
-                        break;
-                  case 'visitor':
-                        Visitor.create({
-                           'visitorID':ID 
-                        });
-                        reply(JSON.stringify('Registo Bem Sucedido'));
-                        break;
-                  case 'organizer':
-                        Organizer.create({
-                           'visitorID':ID 
-                        });
-                        reply(JSON.stringify('Registo Bem Sucedido'));
-                        break;
-              }
-           }
-       }     
+        method: 'POST',
+        path: '/register',
+        handler: function(request, reply) {
+            return sequelize.transaction(function(t) {
+                if(!request.payload.email || !request.payload.password || !request.payload.type){
+                    throw new Error('Missing critical fields');
+                }
+                else{
+                    var passHash=crypto.createHash('sha512');
+                    var ID=uuid.v4();
+                    passHash.update(request.payload.password);
+                    return User.create({
+                        'userID':ID,
+                        'email':request.payload.email,
+                        'password':passHash.digest(),
+                        'type':request.payload.type
+                    }, {transaction: t})
+                        .then(function(user) {
+                            switch (request.payload.type){
+                                case 'company':
+                                    var obj = {
+                                        'companyID':user.userID,
+                                        'visitorCounter': 0
+                                    };
+
+                                    if(request.payload.address)
+                                        obj['address']=request.payload.address;
+
+                                    if(request.payload.companyName)
+                                        obj['companyName']=request.payload.companyName;
+
+                                    if(request.payload.website)
+                                        obj['website']=request.payload.website;
+
+                                    return Company.create(obj, {transaction: t});
+
+                                    break;
+                                case 'visitor':
+                                    return Visitor.create({
+                                        'visitorID':user.userID
+                                    }, {transaction: t});
+
+                                    break;
+                                case 'organizer':
+                                    return Organizer.create({
+                                        'organizerID':user.userID
+                                    }, {transaction: t});
+
+                                    break;
+                            }
+                        })
+                }})
+                .then(function(result) {
+                    reply(JSON.stringify('Registo Bem Sucedido'));
+                })
+                .catch(function(error) {
+                    reply(Boom.badRequest(error.message));
+                });
+        }
     });
 };
