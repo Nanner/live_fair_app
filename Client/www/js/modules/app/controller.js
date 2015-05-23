97a1033a3cccc527bf640ba8f375596178ca16b9
Daniel Moreira <danieljlmoreira@gmail.com>
2015-05-23 02:24:50 +0100

    [Client] Diferentiation beetween a company profile and own profile done, some things still need to be polished

--- Client/www/js/modules/app/controller.js
+++ Client/www/js/modules/app/controller.js
@@ -9,14 +9,8 @@ module.controller('toogleCtrl', function($scope, $ionicSideMenuDelegate, $rootSc
         {path: "menu.register", name: "Registo"},
         {path: "menu.listfairs", name: "Feiras"},
         {path: "menu.searchFairs", name: "Pesquisa"},
-<<<<<<< HEAD
-        {path: "menu.ownProfile", name: "Profile"}];
-=======
         {path: "menu.settings", name: $translate.instant('settings')}
     ];
-
-
->>>>>>> 0f1c0cc2682b370cecc6ce3b4780168fbbb0307a
 
     $scope.toggleLeft = function() {
         $ionicSideMenuDelegate.toggleLeft();
