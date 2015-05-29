629e4c7c8d11f088789e2aef9a87dd8f6844a50b
Diogo Santos <diogo.rgsantos@gmail.com>
2015-05-29 23:21:32 +0100

    [Client] Fixed scope problems with the login controller

--- Client/www/js/modules/login/controller.js
+++ Client/www/js/modules/login/controller.js
@@ -1,16 +1,17 @@
 var module = angular.module('loginModule');
 
 module.controller('loginCtrl', function ($scope, $state, $stateParams, liveFairApi, $localStorage, utils, $ionicHistory) {
-    $scope.username = "";
-    $scope.password = "";
+    $scope.user = {};
+    $scope.user.username = "";
+    $scope.user.password = "";
 
     $scope.letMeRegister = function() {
         $state.transitionTo('menu.register', $stateParams, { reload: true, inherit: false, notify: true });
     };
     
     $scope.submitLogin = function() {
-        var passwordEncrypted = CryptoJS.SHA256($scope.password).toString();
-        liveFairApi.login($scope.username, passwordEncrypted);
+        var passwordEncrypted = CryptoJS.SHA256($scope.user.password).toString();
+        liveFairApi.login($scope.user.username, passwordEncrypted);
 
         $scope.$on('event:auth-loginConfirmed', function() {
             $ionicHistory.nextViewOptions({
