578a4f24c422c6397824089e06d0edf33b25c04b
PMRocha <ei11078@fe.up.pt>
2015-05-29 16:31:36 +0100

    [Server] fixed register websiter regex

--- Server/app/routes/user.js
+++ Server/app/routes/user.js
@@ -18,7 +18,7 @@ var RegisterSchema = Joi.object().keys({
     password: Joi.string().alphanum().required(),
     type: Joi.string().regex(/visitor|company|organizer/).required(),
     address: Joi.string(),
-    website: Joi.string().regex(/^(http(?:s)?\:\/\/[a-zA-Z0-9]+(?:(?:\.|\-)[a-zA-Z0-9]+)+(?:\:\d+)?(?:\/[\w\-]+)*(?:\/?|\/\w+\.[a-zA-Z]{2,4}(?:\?[\w]+\=[\w\-]+)?)?(?:\&[\w]+\=[\w\-]+)*)$/),
+    website: Joi.string().regex(/^((http(?:s)?\:\/\/)?[a-zA-Z0-9]+(?:(?:\.|\-)[a-zA-Z0-9]+)+(?:\:\d+)?(?:\/[\w\-]+)*(?:\/?|\/\w+\.[a-zA-Z]{2,4}(?:\?[\w]+\=[\w\-]+)?)?(?:\&[\w]+\=[\w\-]+)*)$/),
     companyName: Joi.string()
 });
 
